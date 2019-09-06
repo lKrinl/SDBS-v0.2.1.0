beae7f66a999f2fd775f73e63e5bbde044a5fe28
lKrinl <32423544+lKrinl@users.noreply.github.com>
2019-09-06 14:21:26 -0500

    Create CoreAI.cs

---
+++ CoreAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Your_NPC_AI : CoreAI
+{
+    protected void Start()
+    {
+        CoreAIStart(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // insert your own code here
+    }
+}
