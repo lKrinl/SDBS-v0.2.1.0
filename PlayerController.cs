a6dabf034adc07c5973191dda7140513bfcdecdb
lKrinl <32423544+lKrinl@users.noreply.github.com>
2019-09-11 13:44:02 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class ExampleClass : MonoBehaviour
+public class PlayerControls : MonoBehaviour
 {
     CharacterController characterController;
 
@@ -10,15 +10,44 @@ public class ExampleClass : MonoBehaviour
     public float gravity = 20.0f;
 
     private Vector3 moveDirection = Vector3.zero;
+    private GameObject GameBall;
+    private List<Vector3> velocities;
+    private int velocityArraySize = 10;
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        velocities = new List<Vector3>();
+        GameBall = GameObject.Find("GameBall");
+        GameState.playerStartingPosition = Camera.main.transform.position;
+        GameState.playerPosition = GameState.playerStartingPosition;
     }
 
     void Update()
     {
         Vector3 pos = transform.position;
         pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime; 
+        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+         float deltaX = GameState.playerPosition.x - GameState.playerStartingPosition.x;
+        Vector3 velocity = (newPosition - GameState.playerPosition) / Time.deltaTime;
+        velocities.Insert(0, velocity);
+        if (velocities.Count > velocityArraySize) velocities.RemoveAt(velocityArraySize);
+        Vector3 avgVelocity = new Vector3(0,0,0);
+        for (int i = 0; i < velocities.Count; i++) {
+            avgVelocity += velocities[i] / velocities.Count;
+        }
+        GameState.playerSmoothVelocity = avgVelocity;
+        GameState.playerVelocity = velocity;
+        GameState.playerPosition = newPosition;
+        transform.position = GameState.playerPosition;
+        /* Quaternion ballQuat =  Quaternion.LookRotation(
+            ball.transform.position - GameState.playerPosition - 2.0f * Vector3.forward,
+            Vector3.up
+        );*/
+        Quaternion targetQuat = Quaternion.LookRotation(
+            GameState.mouseTarget - GameState.playerPosition,
+            Vector3.up
+        );
+    }
+}
 
