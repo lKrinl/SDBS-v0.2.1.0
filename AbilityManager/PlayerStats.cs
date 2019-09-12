8d022eca908337d72546e116b7822bc7f5f0689c
xDevilKrinx <32423544+lKrinl@users.noreply.github.com>
2019-09-11 20:56:36 -0500

    Create PlayerStats.cs

---
+++ AbilityManager/PlayerStats.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class PlayerStats : AgentStats {
+
+    private void Start()
+    {
+        ApplyStatModifiers();
+    }
+
+    public override void ApplyStatModifiers()
+    {
+        //Apply Modifiers to Variables
+        maxHealth *= (int)statModifiers[Stats.MaxHealthModifier].StatValue;
+        maxStamina *= (int)statModifiers[Stats.MaxHealthModifier].StatValue;
+        sqrMaxTargetDistance *= statModifiers[Stats.SightRange].StatValue;
+        speed *= statModifiers[Stats.MovementSpeed].StatValue;
+        turnSpeed *= statModifiers[Stats.MovementSpeed].StatValue;
+        agro *= statModifiers[Stats.Notoriety].StatValue;
+
+        CurrentHealth = maxHealth;
+        CurrentStamina = maxStamina;
+    }
+
+    public override void InvokeDeathEvent()
+    {
+        GameManager.Instance.ExitToMenu();
+    }
+}
