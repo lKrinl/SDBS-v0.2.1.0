a4f93b3facb426780c4e68633966e579e364d654
lKrinl <32423544+lKrinl@users.noreply.github.com>
2019-09-11 19:43:24 -0500

    Create CollisionResult.cs

---
+++ CollisionSystem/CollisionResult.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Peril.Physics
+{
+    public struct CollisionResult
+    {
+        public bool Collides;
+        public Vector3 Normal;
+        public float Penetration;
+        public CollisionType Type;
+        public bool First;
+    }
+}
