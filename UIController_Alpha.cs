86b67ad3f3143649e427454ad4f2e4c7c3c8cdec
xDevilKrinx <32423544+lKrinl@users.noreply.github.com>
2019-09-12 13:50:45 -0500

    Update UIController_Alpha.cs

--- UIController_Alpha.cs
+++ UIController_Alpha.cs
@@ -31,70 +31,9 @@ public class UIController : MonoBehaviour
         musicSource.volume = 0.5f;
         musicSource.Play();
     }
-    public void startNewGame() {
-        GameState.resetGame();
-        dynamicObjects.SetActive(true);
-        GameState.gamePaused = false;
-        hidePanels();
-    }
-    public void startNextRound() {
-        //dynamicObjects.SetActive(true);
-        GameState.gamePaused = false;
-        GameState.resetRound();
-        hidePanels();
-    }
-    public void exitSummary() {
-        hidePanels();
-        showPanel("MainPanel");
-    }
-    public void hidePanels() {
-        foreach (Transform child in transform) {
-            child.gameObject.SetActive(false);
-        }
-    }
-    private void OnEnable() {
-        EventManager.StartListening("END_GAME", endGame);
-        EventManager.StartListening("NEXT_ROUND", nextRound);
-    }
-    private void OnDisable() {
-        EventManager.StopListening("END_GAME", endGame);
-        EventManager.StopListening("NEXT_ROUND", nextRound);
-    }
-    private void showPanel(string name) {
-        gameObject.transform.Find(name).gameObject.SetActive(true);
-    }
-    private void nextRound() {
-        //dynamicObjects.SetActive(false);
-        GameState.gamePaused = true;
-        hidePanels();
-        showPanel("NextRoundPanel");
-        setNextRoundText();
-        StartCoroutine(waitForNextRound());
-        audioSource.PlayOneShot(sounds[0]);
-    }
-    private void setGameWinnerText() {
-        gameWinnerText.GetComponent<Text>().text = 
-            "You " + (GameState.playerScore < GameState.opponentScore ? "lose" : "win") + "! (<b>" + GameState.playerScore + "-" + GameState.opponentScore + "</b>)";
-    }
-    private void setNextRoundText() {
-        roundScoreText.GetComponent<Text>().text = 
-            "Current score: <b>" + GameState.playerScore + "-" + GameState.opponentScore + "</b>";
-        roundWinnerText.GetComponent<Text>().text = 
-            "You" + (GameState.lastRoundWinner == 0 ? " win!" : " lose!");
-    }
-    private IEnumerator waitForNextRound() {
-        yield return new WaitForSeconds(3.0f);
-        startNextRound();
-    }
-    private void endGame() {
-        audioSource.PlayOneShot(sounds[1]);
-        hidePanels();
-        showPanel("YouWinPanel");
-        setGameWinnerText();
-        //GameState.resetGame();
-        //dynamicObjects.SetActive(false);
-        GameState.gamePaused = true;
-    }
+
     private void Update() {
     }
 }
+
+////To do/////
