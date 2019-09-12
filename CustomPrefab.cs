f0c4b9a881bfbd286363a77007980d478059db55
xDevilKrinx <32423544+lKrinl@users.noreply.github.com>
2019-09-12 13:51:47 -0500

    Update CustomPrefab.cs

--- CustomPrefab.cs
+++ CustomPrefab.cs
@@ -28,16 +28,13 @@ public class CustomPrefab {
                 //Separate the component name from the header
                 string componentName = dataLines[dataPointer].Substring(dataLines[dataPointer].IndexOf(":") + 1)
                 dataPointer++;
-                //Unity uses C# reflection and allows us to simply pass the string name of the script we want to
                 CustomComponentBase c = go.AddComponent(componentName) as CustomComponentBase;
                 
-                //This is similar to calling go.AddComponent("ComponentA");
                 while(dataLines[dataPointer].Length < 1) {
                     dataPointer++;//Clear any white space after the component token
                 }
                 
                 if(c !=null){
-                    //Special components should implement the ComponentType class
                     //Pass the dataPointer as a (ref) to continue with any modifications
                     c.SetData(dataLines, ref dataPointer);
                 } else {
