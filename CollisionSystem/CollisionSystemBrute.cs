8d13e75b3498ac195fdc5b34dfe031b21e4bf869
lKrinl <32423544+lKrinl@users.noreply.github.com>
2019-09-11 19:42:42 -0500

    Create CollisionSystemBrute.cs

---
+++ CollisionSystem/CollisionSystemBrute.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace Peril.Physics
+{
+    //Brute force collision detection algorithm.
+    public class CollisionSystemBrute : CollisionSystem
+    {
+
+        ///// Methods /////
+        public override void DetectBodyVsBody()
+        {
+            int count = bodyList.Count;
+            for (var i = 0; i < count; i++)
+            {
+                if (bodyList[i].Sleeping)
+                    continue;
+
+                for (var j = i + 1; j < count; j++)
+                {
+                    if (bodyList[j].Sleeping)
+                        continue;
+
+                    Test(bodyList[i], bodyList[j]);
+                }
+            }
+        }
+
+        public override bool LineOfSight(Vector3 start, Vector3 end)
+        {
+            for (var i = 0; i < bodyList.Count; i++)
+            {
+                if (CollisionTest.SegmentIntersects(bodyList[i].CollisionShape, start, end))
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
+
