159d12b72470bac16bf483deca286d40ebc9abc6
xDevilKrinx <32423544+lKrinl@users.noreply.github.com>
2019-09-11 20:38:28 -0500

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,53 +1,316 @@
+using System;
+using System.Linq;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.EventSystems;
 
-public class PlayerControls : MonoBehaviour
-{
-    CharacterController characterController;
+public class PlayerController : MonoBehaviour {
 
-    public float speed = 6.0f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+    public event Action OnEscapeKey;    //Used to signify when the "Escape" button has been pressed - clears UI, pauses the game, etc...
 
-    private Vector3 moveDirection = Vector3.zero;
-    private GameObject GameBall;
-    private List<Vector3> velocities;
-    private int velocityArraySize = 10;
+    // ----- References ----- //
+    private PauseMenu pauseMenu;
 
-    void Start()
+    // ----- Components ----- //
+    public GameObject inventoryUI;
+    public StatUIManager statUI;
+    public CharacterSheetManager characterSheet;
+
+    private StoryManager storyManager;
+    private AbilityComponent abilityComponent;
+    private Projector targetIndicator;
+    private Camera cam;
+    private AgentStats playerStats;
+
+    //Interaction
+    private Targetable target;
+    public Targetable Target { get { return target; } }
+
+    public float Speed { get { return playerStats.speed; } }
+    public float TurnSpeed { get { return playerStats.turnSpeed; } }
+
+    //Utility
+    private float checkStuckInterval = 5f;
+    private float resetRotationTime;
+
+    //References & initialisation - Start is once scripts are definitely enabled - Awake the GOs are all enabled
+    private void Awake()    
     {
-        characterController = GetComponent<CharacterController>();
-        velocities = new List<Vector3>();
-        GameBall = GameObject.Find("GameBall");
-        GameState.playerStartingPosition = Camera.main.transform.position;
-        GameState.playerPosition = GameState.playerStartingPosition;
+        abilityComponent = GetComponent<AbilityComponent>();
+        targetIndicator = GetComponentInChildren<Projector>();
+        cam = Camera.main;
+        playerStats = GetComponent<AgentStats>();
+        pauseMenu = FindObjectOfType<PauseMenu>();
+
+        target = null;
+
+        //Refresh keybinds
+        GameMetaInfo.keybindActions = new System.Collections.Generic.Dictionary<KeyCode, Action>();
+        GameMetaInfo.abilityKeybinds = new System.Collections.Generic.Dictionary<Abilities, KeyCode>();
+        //Abilities
+        GameMetaInfo.SetAbilityKeybindAction(Abilities.Zap, KeyCode.Alpha1, new Action(() => UseAbility(Abilities.Zap, target)));
+        GameMetaInfo.SetAbilityKeybindAction(Abilities.Confuse, KeyCode.Alpha2, new Action(() => UseAbility(Abilities.Confuse, target)));
+        GameMetaInfo.SetAbilityKeybindAction(Abilities.Vortex, KeyCode.Alpha3, new Action(() => UseAbility(Abilities.Vortex, target)));
+        GameMetaInfo.SetAbilityKeybindAction(Abilities.Singularity, KeyCode.Alpha4, new Action(() => UseAbility(Abilities.Singularity, target)));
+        GameMetaInfo.SetAbilityKeybindAction(Abilities.Heal, KeyCode.Alpha5, new Action(() => UseAbility(Abilities.Heal, target)));
+
+        //General & UI
+        GameMetaInfo.keybindActions.Add(KeyCode.F, Interact);
+        GameMetaInfo.keybindActions.Add(KeyCode.Escape, TriggerEscapeAction);
+        GameMetaInfo.keybindActions.Add(KeyCode.I, new Action(() => inventoryUI.gameObject.SetActive(!inventoryUI.activeSelf)));
+        GameMetaInfo.keybindActions.Add(KeyCode.J, TriggerJournalLoad);
+        GameMetaInfo.keybindActions.Add(KeyCode.C, characterSheet.OpenOrClose);
+
+        //Camera
+        GameMetaInfo.keybindActions.Add(KeyCode.LeftAlt, cam.GetComponent<PlayerCamera>().SwitchCameraMode);
     }
 
-    void Update()
+    //EVENT SUBSCRIPTIONS & Script property references
+    private void Start()
     {
-        Vector3 pos = transform.position;
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-         float deltaX = GameState.playerPosition.x - GameState.playerStartingPosition.x;
-        Vector3 velocity = (newPosition - GameState.playerPosition) / Time.deltaTime;
-        velocities.Insert(0, velocity);
-        if (velocities.Count > velocityArraySize) velocities.RemoveAt(velocityArraySize);
-        Vector3 avgVelocity = new Vector3(0,0,0);
-        for (int i = 0; i < velocities.Count; i++) {
-            avgVelocity += velocities[i] / velocities.Count;
+        storyManager = StoryManager.Instance;
+        //EVENT: onTargetDestroyed - Invoked by target of player
+        PlayerManager.Instance.onTargetDestroyed += RegisterKill;    //Delegate: Register to reset target indicator when target is destroyed 
+    }
+
+    //UNSUBSCRIBE EVENTS
+    private void OnDisable()
+    {
+        PlayerManager.Instance.onTargetDestroyed -= RegisterKill;
+    }
+
+    //Call Handle methods
+    private void Update () {
+        HandleDirectionInput();
+        HandleTargeting();
+        HandleKeyboardInput();
+        HandleConversationInput();
+
+        //CheckStuck();//TODO: Test
+    }
+    
+    #region Event Triggers - Escape & Mission Journal
+    //Used as a keybind action for the "Escape" key event
+    public void TriggerEscapeAction()
+    {
+        if (abilityComponent.AbilityAimingActive)
+        {
+            abilityComponent.CancelAoE();
         }
-        GameState.playerSmoothVelocity = avgVelocity;
-        GameState.playerVelocity = velocity;
-        GameState.playerPosition = newPosition;
-        transform.position = GameState.playerPosition;
-        /* Quaternion ballQuat =  Quaternion.LookRotation(
-            ball.transform.position - GameState.playerPosition - 2.0f * Vector3.forward,
-            Vector3.up
-        );*/
-        Quaternion targetQuat = Quaternion.LookRotation(
-            GameState.mouseTarget - GameState.playerPosition,
-            Vector3.up
-        );
+        else
+        {
+            if (OnEscapeKey != null) OnEscapeKey.Invoke();
+        }
+    }
+
+    //Open & Update the Mission Journal - event
+    public void TriggerJournalLoad()
+    {
+        if (MissionManager.Instance.onJournalOpened != null) MissionManager.Instance.onJournalOpened.Invoke();
+    }
+    #endregion
+
+    //Attempts to converse with story NPCs or pick up objects
+    public void Interact()
+    {
+        switch (target.targetType)
+        {
+            case TargetType.Item:
+                target.GetComponent<Item>().AddToInventory();
+                SetTargetIndicatorPos(false);
+                break;
+
+            case TargetType.Story:
+                storyManager.AttemptToConverse((InteractableNPC)target);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    //Checks target can be attacked & handles "self abilities"
+    public void UseAbility(Abilities abil, Targetable target = null)
+    {
+        AbilityUI.Instance.ChangeSelectedDisplay(abil);
+
+        if (GameMetaInfo._Is_AoE_Ability(abil))
+        {
+            abilityComponent.PlayerUseAoE(abil);
+        }
+        else if (GameMetaInfo._Is_Defense_Ability(abil))
+        {
+            abilityComponent.PlayerUseInstant(abil, transform);
+        }
+        else if (target != null)
+        {
+            if ((target.GetComponent<Targetable>().targetType == TargetType.Enemy || target.tag.Equals(GameMetaInfo._TAG_SHOOTABLE_BY_PLAYER)) && !GameMetaInfo._Is_Defense_Ability(abil))
+            {
+                abilityComponent.PlayerUseInstant(abil, target.transform);
+            }
+        }
+        else
+        {
+            abilityComponent.SelectAbility(abil);
+        }
+    }
+
+    //Simple axis movement
+    private void HandleDirectionInput()
+    {
+        float x = Input.GetAxis("Horizontal") * TurnSpeed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    //Sends a raycast to detect "Targetable" type objects on the "Object" layer, ignoring trigger colliders
+    private void HandleTargeting()
+    {
+        if (target == null) statUI.ClearTarget();
+        //Must be within x metres - using sqr values since getting root is expensive
+        if(target != null && 
+            (target.transform.position - transform.position).sqrMagnitude > playerStats.sqrMaxTargetDistance)
+        {
+            targetIndicator.enabled = false;
+            target = null;
+            StoryManager.Instance.CloseConversation();
+        }
+
+        //TARGET OPERATION (LMB)
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit pointHit;
+
+            //Raycast
+            if(Physics.Raycast(ray, out pointHit, 100f, LayerMask.GetMask("Object"), QueryTriggerInteraction.Ignore))    //If the raycast hits something within 100
+            {
+                //If within range
+                if ((pointHit.transform.position - transform.position).sqrMagnitude < playerStats.sqrMaxTargetDistance)
+                {
+                    if (pointHit.transform.GetComponent<Targetable>() != null)  
+                    {
+                        Targetable t = pointHit.transform.GetComponent<Targetable>();
+                        if (t == null) t = pointHit.transform.GetComponentInParent<Targetable>();
+                        if (t == null) return;
+
+                        TargetType currentTargetType = t.targetType;
+
+                        switch (currentTargetType)
+                        {
+                            case TargetType.Item:
+                                target = pointHit.transform.GetComponent<Item>();
+                                SetTargetIndicatorPos(true);
+                                break;
+
+                            case TargetType.Enemy:
+                                target = pointHit.transform.GetComponent<Enemy>();
+                                SetTargetIndicatorPos(true);
+                                statUI.SetTarget(target.GetComponent<AgentStats>());
+                                break;
+
+                            case TargetType.Story:
+                                target = pointHit.transform.GetComponent<InteractableNPC>();
+                                SetTargetIndicatorPos(true);
+                                break;
+
+                            default:
+                                SetTargetIndicatorPos(false);
+                                break;
+                        }
+                    }
+                }
+            }
+            //Check if we have clicked UI
+            else if(EventSystem.current.IsPointerOverGameObject())
+            {
+                //Debug.Log("Hit ui");
+                return; //Don't lose target if mouse clicking UI
+            }
+            //If nothing clicked, reset the target
+            else
+            {
+                SetTargetIndicatorPos(false);
+            }
+        }
+    }
+
+    //Use the keybind dictionary to perform actions
+    private void HandleKeyboardInput()
+    {
+        for (int i = 0; i < Enum.GetValues(typeof(KeyCode)).Cast<int>().Last(); i++)
+        {
+            KeyCode code = (KeyCode)i;
+            if (Input.GetKeyDown(code) && GameMetaInfo.keybindActions.ContainsKey(code))
+            {
+                try
+                {
+                    GameMetaInfo.keybindActions[code]();
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e.StackTrace);
+                    Debug.Log("Either Keybind not able to trigger or an exception occurred");
+                }
+            }
+        }
+    }
+
+    //Make story choices depending on keyboard input
+    private void HandleConversationInput()
+    {
+        if (target != null)
+        {
+            if (StoryManager.Instance.StoryInputEnabled)
+            {
+                int playerChoice;
+                bool correctInput = int.TryParse(Input.inputString, out playerChoice);
+                if (correctInput)
+                {
+                    StoryManager.Instance.Choice(playerChoice);
+                }
+            }
+        }
+    }
+
+    //Registers a kill and then resets the target
+    private void RegisterKill()
+    {
+        SetTargetIndicatorPos(false);
+    }
+
+    //Places a projector on the current target
+    private void SetTargetIndicatorPos(bool dontResetTarget)
+    {
+        float height;
+
+        if (dontResetTarget)
+        {
+            height = target.transform.localScale.y * 5;
+
+            targetIndicator.transform.SetParent(target.transform);
+            targetIndicator.transform.position = target.transform.position + new Vector3(0f, height);
+            targetIndicator.enabled = true;
+        }
+        else
+        {
+            target = null;
+            targetIndicator.transform.SetParent(transform);
+            targetIndicator.transform.position = transform.position;
+            targetIndicator.enabled = false;
+        }
+    }
+
+    private void CheckStuck()
+    {
+        //TODO: test
+        if(transform.localEulerAngles.x > 50f)
+        {
+            resetRotationTime = Time.time + checkStuckInterval;
+            if(Time.time > resetRotationTime)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+            }
+        }
     }
 }
-
